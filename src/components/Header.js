8075abbca8df83a56e2dcbec36808add6a6cb4ce
Karam K. Afandi <77574018+karamlyy@users.noreply.github.com>
2023-11-13 16:30:21 +0400

    hamburger menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,37 @@
-// Header.js
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.css';
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <header className="header">
-      <nav className="header-nav">
-        <ul className="header-list">
-          <li className="header-item"><a href="#about">Haqqımda</a></li>
-          <li className="header-item"><a href="#services">Xidmətlər</a></li>
-          <li className="header-item"><a href="#education">Təhsil</a></li>
-          <li className="header-item"><a href="#contact">Əlaqə</a></li>
-        </ul>
-      </nav>
-      
+    <header className={`header ${isMenuOpen ? 'fixed-header' : ''}`}>
+      <div className="header-container">
+        <h1>Dr. Efendiyev</h1>
+        <div className={`menu-icon ${isMenuOpen ? 'open' : ''}`} onClick={toggleMenu}>
+          <div className="bar"></div>
+          <div className="bar"></div>
+          <div className="bar"></div>
+        </div>
+        <nav className={`nav-menu ${isMenuOpen ? 'open' : ''}`}>
+          <a href="#about" onClick={toggleMenu}>
+            Haqqımda
+          </a>
+          <a href="#services" onClick={toggleMenu}>
+            Xidmətlər
+          </a>
+          <a href="#education" onClick={toggleMenu}>
+            Təhsil
+          </a>
+          <a href="#contact" onClick={toggleMenu}>
+            Əlaqə
+          </a>
+        </nav>
+      </div>
     </header>
   );
 };
